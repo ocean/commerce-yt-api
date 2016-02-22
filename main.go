6e85f95d6563a387668689d2e45c5b43ae5115ed
Drew Robinson <drew.robinson@gmail.com>
2016-02-22 14:53:23 +0800

    Added another test URL.

--- main.go
+++ main.go
@@ -1,58 +1,75 @@
 package main
 
 import (
-  "log"
-  "net/http"
-  "os"
-  // "fmt"
-  // "encoding/json"
-  "io/ioutil"
-  "github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	// "fmt"
+	// "encoding/json"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
 )
 
 var (
-  port = 5000
+	port = 5000
 )
 
 func main() {
 
-  var port string
-  if (os.Getenv("PORT") == "") {
-    port = "5000"
-  } else {
-    port = os.Getenv("PORT")
-  }
+	var port string
+	if os.Getenv("PORT") == "" {
+		port = "5000"
+	} else {
+		port = os.Getenv("PORT")
+	}
 
-  if port == "" {
+	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
-  router := gin.Default()
+	router := gin.Default()
 
-  router.GET("/ping", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message": "pong",
-    })
-  })
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
-  router.GET("/fuel/:suburb", func(c *gin.Context) {
-    suburb := c.Param("suburb")
-    resp, err := http.Get("http://nfwws.herokuapp.com/v1/s/" + suburb)
-    if err != nil {
-      log.Fatal(err)
-    }
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
+  router.GET("/form-submissions", func(c *gin.Context) {
+		resp, err := http.Get("http://forms.commerce.wa.gov.au/api/forms/results?token=ZuesbwqGhQMTxTbytbj7qrBWR_E84lTCSYLiVL1yk8Q")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
 
-    // fmt.Printf("%s", body)
+		// fmt.Printf("%s", body)
 
-    var jsonContentType = []string{"application/json; charset=utf-8"}
-    writeContentType(c.Writer, jsonContentType)
-    var out string =  string(body[:])
-    c.String(http.StatusOK, out)
-  })
+		var jsonContentType = []string{"application/json; charset=utf-8"}
+		writeContentType(c.Writer, jsonContentType)
+		var out string = string(body[:])
+		c.String(http.StatusOK, out)
+    // c.JSON(http.StatusOK, body)
+	})
 
-  router.Run(":" + port)
+	router.GET("/fuel/:suburb", func(c *gin.Context) {
+		suburb := c.Param("suburb")
+		resp, err := http.Get("http://nfwws.herokuapp.com/v1/s/" + suburb)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+
+		// fmt.Printf("%s", body)
+
+		var jsonContentType = []string{"application/json; charset=utf-8"}
+		writeContentType(c.Writer, jsonContentType)
+		var out string = string(body[:])
+		c.String(http.StatusOK, out)
+	})
+
+	router.Run(":" + port)
 }
 
 func writeContentType(w http.ResponseWriter, value []string) {
