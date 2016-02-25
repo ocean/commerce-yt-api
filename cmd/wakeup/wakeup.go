8486331a73166594376c248f0e752d2ca31066ae
Drew Robinson <drew.robinson@gmail.com>
2016-02-25 11:55:24 +0800

    Added tiny wakeup command script.

---
+++ cmd/wakeup/wakeup.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "os"
+  "log"
+  "net/http"
+)
+
+func main() {
+  url := os.Getenv("WAKE_UP_URL")
+  if url == "" {
+    log.Fatal("$WAKE_UP_URL must be set.")
+  }
+  resp, err := http.Get(url)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer resp.Body.Close()
+  log.Printf("Status: %s Protocol: %s", resp.Status, resp.Proto)
+}
