72e19daee40fdb106566272d6946ce63213280ee
Drew Robinson <drew.robinson@gmail.com>
2016-06-29 14:50:55 +0800

    - Reorganised into proper main and helper functions for better Go. - Removed unneeded Content-Type helper function, using Gin built-in. - Fancier ping endpoint. - More comments.

--- cmd/commerce-yt-api/main.go
+++ cmd/commerce-yt-api/main.go
@@ -8,14 +8,111 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+// SCALD_YOUTUBE_API Search request
+// https://www.googleapis.com/youtube/v3
+// + /search?key=' . $api_key . '&q=' . $q . '&part=snippet&order=rating&type=video,playlist
+func searchRequest(c *gin.Context) {
+	key := c.Query("key")
+	q := url.QueryEscape(c.Query("q"))
+	suffix := "&part=snippet&order=rating&type=video,playlist"
+	log.Printf("search query = %s", q)
+	resp, err := http.Get(fmt.Sprintf("https://www.googleapis.com/youtube/v3/search?key=%s&q=%s%s", key, q, suffix))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	// fmt.Printf("%s", body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.Header("Content-Type", "application/json; charset=utf-8")
+	var out = string(body[:])
+	c.String(http.StatusOK, out)
+}
+
+// SCALD_YOUTUBE_API RSS Feed request
+// https://www.googleapis.com/youtube/v3
+// + /videos?id=' . $id . '&key=' . $api_key . '&part=snippet
+func rssFeedRequest(c *gin.Context) {
+	id := c.Query("id")
+	key := c.Query("key")
+	suffix := "&part=snippet"
+	log.Printf("video id = %s", id)
+	resp, err := http.Get(fmt.Sprintf("https://www.googleapis.com/youtube/v3/videos?id=%s&key=%s%s", id, key, suffix))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	// fmt.Printf("%s", body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.Header("Content-Type", "application/json; charset=utf-8")
+	var out = string(body[:])
+	c.String(http.StatusOK, out)
+}
+
+// SCALD_YOUTUBE_WEB request
+// https://www.youtube.com/watch
+// + /watch?v=' . $id
+func watchRequest(c *gin.Context) {
+	id := c.Query("v")
+	log.Printf("video id = %s", id)
+	resp, err := http.Get("https://www.youtube.com/watch?v=" + id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.Header("Content-Type", "text/html; charset=utf-8")
+	var out = string(body[:])
+	c.String(http.StatusOK, out)
+}
+
+// SCALD_YOUTUBE_THUMBNAIL request
+// https://i.ytimg.com
+func thumbnailRequest(c *gin.Context) {
+	q := c.Query("q")
+	log.Printf("query url = %s", q)
+	resp, err := http.Get(q)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO: add in content type checking
+	if strings.HasSuffix(q, "jpg") {
+		c.Data(http.StatusOK, "image/jpeg", body)
+	} else if strings.HasSuffix(q, "png") {
+		c.Data(http.StatusOK, "image/png", body)
+	} else {
+		c.String(http.StatusForbidden, "403 Forbidden: Image requests only.")
+	}
+}
+
+// Set up required variables
 var (
 	port = 5000
 )
 
+// Start the main function
 func main() {
 
 	var port string
@@ -29,8 +126,10 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
+	// Call in the gin router
 	router := gin.Default()
 
+	// Simple front page, using a template for fun.
 	router.LoadHTMLGlob("templates/*")
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
@@ -40,10 +139,15 @@ func main() {
 		})
 	})
 
+	// Return ping requests with a nice timestamp.
 	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+		var resp struct {
+			Response  string    `json:"response"`
+			Timestamp time.Time `json:"timestamp"`
+		}
+		resp.Response = "pong"
+		resp.Timestamp = time.Now().Local()
+		c.JSON(http.StatusOK, resp)
 	})
 
 	// ----- ACTUAL REAL THINGS
@@ -51,99 +155,21 @@ func main() {
 	// SCALD_YOUTUBE_API Search request
 	// https://www.googleapis.com/youtube/v3
 	// + /search?key=' . $api_key . '&q=' . $q . '&part=snippet&order=rating&type=video,playlist
-	router.GET("/v1/search", func(c *gin.Context) {
-		key := c.Query("key")
-		q := url.QueryEscape(c.Query("q"))
-		suffix := "&part=snippet&order=rating&type=video,playlist"
-		log.Printf("search query = %s", q)
-		resp, err := http.Get(fmt.Sprintf("https://www.googleapis.com/youtube/v3/search?key=%s&q=%s%s", key, q, suffix))
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		// fmt.Printf("%s", body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var jsonContentType = []string{"application/json; charset=utf-8"}
-		writeContentType(c.Writer, jsonContentType)
-		var out = string(body[:])
-		c.String(http.StatusOK, out)
-	})
+	router.GET("/v1/search", searchRequest)
 
 	// SCALD_YOUTUBE_API RSS Feed request
 	// https://www.googleapis.com/youtube/v3
 	// + /videos?id=' . $id . '&key=' . $api_key . '&part=snippet
-	router.GET("/v1/videos", func(c *gin.Context) {
-		id := c.Query("id")
-		key := c.Query("key")
-		suffix := "&part=snippet"
-		log.Printf("video id = %s", id)
-		resp, err := http.Get(fmt.Sprintf("https://www.googleapis.com/youtube/v3/videos?id=%s&key=%s%s", id, key, suffix))
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		// fmt.Printf("%s", body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var jsonContentType = []string{"application/json; charset=utf-8"}
-		writeContentType(c.Writer, jsonContentType)
-		var out = string(body[:])
-		c.String(http.StatusOK, out)
-	})
+	router.GET("/v1/videos", rssFeedRequest)
 
 	// SCALD_YOUTUBE_WEB request
 	// https://www.youtube.com/watch
 	// + /watch?v=' . $id
-	router.GET("/v1/watch", func(c *gin.Context) {
-		id := c.Query("v")
-		log.Printf("video id = %s", id)
-		resp, err := http.Get("https://www.youtube.com/watch?v=" + id)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var htmlContentType = []string{"text/html; charset=utf-8"}
-		writeContentType(c.Writer, htmlContentType)
-		var out = string(body[:])
-		c.String(http.StatusOK, out)
-	})
+	router.GET("/v1/watch", watchRequest)
 
 	// SCALD_YOUTUBE_THUMBNAIL request
 	// https://i.ytimg.com
-	router.GET("/v1/thumbnail", func(c *gin.Context) {
-		q := c.Query("q")
-		log.Printf("query url = %s", q)
-		resp, err := http.Get(q)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// TODO: add in content type checking
-		if strings.HasSuffix(q, "jpg") {
-			c.Data(http.StatusOK, "image/jpeg", body)
-		} else if strings.HasSuffix(q, "png") {
-			c.Data(http.StatusOK, "image/png", body)
-		} else {
-			c.String(http.StatusForbidden, "403 Forbidden: Image requests only.")
-		}
-	})
+	router.GET("/v1/thumbnail", thumbnailRequest)
 
 	// ----- SOME TEST THINGS
 	router.GET("/form-submissions", func(c *gin.Context) {
@@ -161,8 +187,7 @@ func main() {
 			log.Fatal(err)
 		}
 
-		var jsonContentType = []string{"application/json; charset=utf-8"}
-		writeContentType(c.Writer, jsonContentType)
+		c.Header("Content-Type", "application/json; charset=utf-8")
 		var out = string(body[:])
 		c.String(http.StatusOK, out)
 	})
@@ -181,18 +206,11 @@ func main() {
 
 		// fmt.Printf("%s", body)
 
-		var jsonContentType = []string{"application/json; charset=utf-8"}
-		writeContentType(c.Writer, jsonContentType)
+		c.Header("Content-Type", "application/json; charset=utf-8")
 		var out = string(body[:])
 		c.String(http.StatusOK, out)
 	})
 
+	// Run, collaborate and listen.
 	router.Run(":" + port)
-}
-
-func writeContentType(w http.ResponseWriter, value []string) {
-	header := w.Header()
-	if val := header["Content-Type"]; len(val) == 0 {
-		header["Content-Type"] = value
-	}
 }
