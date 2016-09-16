4e38bd1199ce987d3ced92ff57bae3efed0697cd
Drew Robinson <drew.robinson@gmail.com>
2016-09-16 17:42:30 +0800

    Starting on some GoConvey tests to expand the test coverage.

--- cmd/commerce-yt-api/main_test.go
+++ cmd/commerce-yt-api/main_test.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"testing"
-	// "net/http"
-	// "log"
+	"net/http"
+	"log"
 	// "net/http/httptest"
 	// "github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestPing(t *testing.T) {
@@ -24,4 +25,19 @@ func TestPing(t *testing.T) {
 	// assert.NoError(t, reqerr)
 	// assert.Equal(t, w.Body.String(), "{\"message\": \"pong\"}")
 	assert.Equal(t, "ping", "ping")
+}
+
+func TestHomePage(t *testing.T)  {
+	// h := httptest.NewRecorder()
+
+	req, err := http.NewRequest("GET", "http://localhost:5000/", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Convey("When you hit the root URL", t, func() {
+		Convey("The result should contain Welcome", t, func() {
+			So(req, ShouldContainSubstring, "Welcome")
+		})
+	})
 }
