658e3e6552b2e55bec8f2a0a1964923fe1812209
Drew Robinson <drew.robinson@gmail.com>
2016-10-10 15:42:19 +0800

    👍 Fixed tests!
    - Got template loading working for home page test.
    - Added lots more tests for HTTP status codes, headers etc.
    - Added new tests for YT thumbnail requests and bad thumbnail requests.

--- cmd/commerce-yt-api/main_test.go
+++ cmd/commerce-yt-api/main_test.go
@@ -4,17 +4,18 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
-	// "github.com/stretchr/testify/assert"
-	. "github.com/smartystreets/goconvey/convey"
-	"log"
+
 	"net/http"
 	"net/http/httptest"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestHomePage(t *testing.T)  {
+func TestHomePage(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 
 	router := gin.Default()
+	router.LoadHTMLGlob("../../templates/*")
 	router.GET("/", home)
 
 	Convey("When you hit the root URL", t, func() {
@@ -22,7 +23,11 @@ func TestHomePage(t *testing.T)  {
 			req, _ := http.NewRequest("GET", "/", nil)
 			resp := httptest.NewRecorder()
 			router.ServeHTTP(resp, req)
-			log.Println(resp)
+			// log.Println(resp)
+			So(resp.Code, ShouldHaveSameTypeAs, 1)
+			So(resp.Code, ShouldEqual, 200)
+			So(resp.HeaderMap, ShouldContainKey, "Content-Type")
+			So(resp.HeaderMap.Get("Content-Type"), ShouldContainSubstring, "text/html")
 			So(resp.Body.String(), ShouldContainSubstring, "Welcome")
 		})
 	})
@@ -39,7 +44,11 @@ func TestPing(t *testing.T) {
 			req, _ := http.NewRequest("GET", "/ping", nil)
 			resp := httptest.NewRecorder()
 			router.ServeHTTP(resp, req)
-			log.Println(resp)
+			// log.Println(resp)
+			So(resp.Code, ShouldHaveSameTypeAs, 1)
+			So(resp.Code, ShouldEqual, 200)
+			So(resp.HeaderMap, ShouldContainKey, "Content-Type")
+			So(resp.HeaderMap.Get("Content-Type"), ShouldContainSubstring, "application/json")
 			So(resp.Body.String(), ShouldContainSubstring, "pong")
 		})
 	})
@@ -57,7 +66,51 @@ func TestWatchRequest(t *testing.T) {
 			resp := httptest.NewRecorder()
 			router.ServeHTTP(resp, req)
 			// log.Println(resp)
+			// log.Println(resp.Status())
+			// log.Println(resp.HeaderMap["Content-Type"])
+			So(resp.Code, ShouldHaveSameTypeAs, 1)
+			So(resp.Code, ShouldEqual, 200)
+			So(resp.HeaderMap, ShouldContainKey, "Content-Type")
+			So(resp.HeaderMap.Get("Content-Type"), ShouldContainSubstring, "text/html")
 			So(resp.Body.String(), ShouldContainSubstring, "PSY - GANGNAM")
+		})
+	})
+}
+
+func TestThumbnailRequest(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	router := gin.Default()
+	router.GET("/v1/thumbnail", thumbnailRequest)
+
+	Convey("When you hit the thumbnail URL for PSY - GANGNAM STYLE", t, func() {
+		Convey("The result should be an image", func() {
+			req, _ := http.NewRequest("GET", "/v1/thumbnail?q=https://i.ytimg.com/vi/9bZkp7q19f0/hqdefault.jpg", nil)
+			resp := httptest.NewRecorder()
+			router.ServeHTTP(resp, req)
+			So(resp.Code, ShouldHaveSameTypeAs, 1)
+			So(resp.Code, ShouldEqual, 200)
+			So(resp.HeaderMap, ShouldContainKey, "Content-Type")
+			So(resp.HeaderMap.Get("Content-Type"), ShouldContainSubstring, "image")
+		})
+	})
+}
+
+func TestBadThumbnailRequest(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	router := gin.Default()
+	router.GET("/v1/thumbnail", thumbnailRequest)
+
+	Convey("When you hit the thumbnail URL looking for some random thing", t, func() {
+		Convey("The result should be a '403 Forbidden' error", func() {
+			req, _ := http.NewRequest("GET", "/v1/thumbnail?q=https://www.google.com.au", nil)
+			resp := httptest.NewRecorder()
+			router.ServeHTTP(resp, req)
+			So(resp.Code, ShouldHaveSameTypeAs, 1)
+			So(resp.Code, ShouldEqual, 403)
+			So(resp.HeaderMap, ShouldContainKey, "Content-Type")
+			So(resp.HeaderMap.Get("Content-Type"), ShouldContainSubstring, "text/plain")
 		})
 	})
 }
